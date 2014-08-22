b877dacf6f35d5a33f0e33d0ddd20a9ec1928a74
Chris Johnson <chris@crisistextline.org>
2014-08-22 17:54:39 -0400

    Whoops, forgot a comment.

--- jobs/new_texters.rb
+++ jobs/new_texters.rb
@@ -11,7 +11,7 @@ base_query = 'SELECT count(*) as count FROM actor a WHERE type != "Internal" AND
 
 def get_data time, query
   query = query % [time.strftime('%Y-%m-%d'), (time + 60*60*24).strftime('%Y-%m-%d')]
-  #rs = DataAccessor.instance.query(query)
+  rs = DataAccessor.instance.query(query)
 
   count = 0
   rs.each_hash do |h|
